9981ea540a2ca5ace11f0827402cfe991eed57a1
Tushar Gaikwad <tushargaikwad220@gmail.com>
2022-07-31 10:23:30 +0530

    Changes in feature 1 new line

--- java1.java
+++ java1.java
@@ -2,7 +2,8 @@ class GitTest1{
 	public static void main(String []args){
 	
 	System.out.println("Hello World");
-	
+	System.out.println("Added new line");
+	System.out.println("Changes done in feature1");
 	}
 	
 }
